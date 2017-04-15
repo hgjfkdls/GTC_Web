9662b7f71bc3bf3526645e3b7c2874ba15e64542
hgjfkdls <fcabedov@gmail.com>
2017-04-15 02:39:27 -0400

    ADD - simple_row.blade

---
+++ resources/views/modulos/correspondencia/partials/simple_row.blade.php
@@ -0,0 +1,44 @@
+<tr data-id="{{ $row->id }}">
+    <td>
+        <div class="btn-group btn-group dropdown">
+            <label class="btn btn-sm btn-default">
+                <input class="btn tag-checkbox" type="checkbox">
+            </label>
+            <button class="btn btn-sm btn-default dropdown-toggle" data-toggle="dropdown">
+                <span class="caret"></span>
+            </button>
+            <ul class="dropdown-menu dropdown-menu-left">
+                <li class="dropdown-header">
+                    <b>Documento</b>
+                </li>
+                <li>
+                    <a href="{{ url()->route('correspondencia.show_doc', [$row->id]) }}" target="_blank">
+                        <span class="glyphicon glyphicon-file"></span>
+                        ver pdf
+                    </a>
+                </li>
+                <li>
+                    <a href="{{ url()->route('correspondencia.show_txt', [$row->id]) }}" target="_blank">
+                        <span class="glyphicon glyphicon-list-alt"></span>
+                        ver texto plano
+                    </a>
+                </li>
+                <li>
+                    <a class="tag-etiquetar">
+                        <span class="glyphicon glyphicon-tags"></span>
+                        Etiquetar
+                    </a>
+                </li>
+                <li>
+                    <a class="tag-descargar">
+                        <span class="glyphicon glyphicon-download-alt"></span>
+                        Descargar
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </td>
+    <td>{{ $row->codigo }}</td>
+    <td>{{ date_format(date_create($row->fecha_receptor), 'Y/m/d') }}</td>
+    <td>{{ $row->nombre }}</td>
+</tr>
--- resources/views/modulos/correspondencia/simple_search.blade.php
+++ resources/views/modulos/correspondencia/simple_search.blade.php
@@ -7,6 +7,26 @@
     @include('modulos.correspondencia.partials.pagination')
     @include('modulos.correspondencia.partials.simple_table')
     @include('modulos.correspondencia.partials.pagination')
+
+    <div id="myModal" class="modal fade" role="dialog">
+        <div class="modal-dialog">
+            <!-- Modal content-->
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    <h4 class="modal-title">Etiquetar</h4>
+                </div>
+                <div class="modal-body">
+                    <p>Contenido</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Aceptar</button>
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Cancelar</button>
+                </div>
+            </div>
+
+        </div>
+    </div>
 @endsection
 @section('script')
     <script>
@@ -14,17 +34,14 @@
 
             $(document).on('click', 'a.tags-etiquetar', function (e) {
                 e.preventDefault();
-                id_arr = [];
-                $('input.tag-checkbox:checked').each(function(){
-                    id_arr.push($(this).parents('tr').data('id'));
-                });
-                console.log('etiquetar seleccion: ' + JSON.stringify(id_arr));
+                $('#myModal').modal({'keyboard': true});
+                console.log('etiquetar seleccion: ' + JSON.stringify(get_idArray()));
             });
 
             $(document).on('click', 'a.tags-descargar', function (e) {
                 e.preventDefault();
                 id_arr = [];
-                $('input.tag-checkbox:checked').each(function(){
+                $('input.tag-checkbox:checked').each(function () {
                     id_arr.push($(this).parents('tr').data('id'));
                 });
                 console.log('descargar seleccion: ' + JSON.stringify(id_arr));
@@ -33,7 +50,8 @@
             $(document).on('click', 'a.tag-etiquetar', function (e) {
                 e.preventDefault();
                 var id = $(this).parents('tr').data('id');
-                console.log('etiquetar: ' + id);
+                $('#myModal').modal({'keyboard': true});
+                console.log('etiquetar: ' + JSON.stringify([id]));
             });
 
             $(document).on('click', 'a.tag-descargar', function (e) {
@@ -65,5 +83,13 @@
             });
 
         });
+
+        function get_idArray() {
+            var id_arr = [];
+            $('input.tag-checkbox:checked').each(function () {
+                id_arr.push($(this).parents('tr').data('id'));
+            });
+            return id_arr;
+        }
     </script>
 @endsection
